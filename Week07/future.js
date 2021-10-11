ba5244e136ff1568f42261a804559798b2a00dff
RicoDixonM <89476300+RicoDixonM@users.noreply.github.com>
2021-10-10 19:16:44 -0600

    Update and rename index.html to future.js

---
+++ Week07/future.js
@@ -0,0 +1,12 @@
+var job = ["Web Developer", "Designer", "Game Developer", "Bartender"];
+var city = ["Dallas", "Austin", "Fort Worth", "Carrollton"];
+var spouse = ["Diana", "Carolina", "Ralph", "Kenya"];
+var num_kids = [2, 3, 1, 0];
+
+var xx = math.floor(math.random() * 4 )
+
+function fortune(job,city,spouse,num_kids) {
+document.write("You will be a " + job + " in " + city + ", and married to " + spouse + " with " + num_kids + " kids.");
+}
+
+fortune(job[xx],city[xx],spouse[xx],num_kids[xx])
